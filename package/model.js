89313563b94247416c07660ae71763b455d14b20
Parth <parth.mthur@gmail.com>
2022-10-09 11:37:10 +0530

    first commit

---
+++ package/model.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var dumbledoresArmySchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    house: String
+});
+module.exports = new mongoose.model('DumbledoresArmy', dumbledoresArmySchema);
